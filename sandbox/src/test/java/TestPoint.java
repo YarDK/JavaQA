a9fcc381f98efb4541e6e1cfccabb5ba9b7c74d1
yaroslavkorotyshov <ydkorotyshov@gmail.com>
2020-07-17 23:21:06 +0300

    Задание 3 Тест нахождения длины

---
+++ sandbox/src/test/java/TestPoint.java
@@ -0,0 +1,29 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestPoint {
+
+    @Test
+    public void testPoint(){
+
+        double point_A_x = 5.0;
+        double point_A_y = 5.0;
+        double point_B_x = -4.0;
+        double point_B_y = -4.0;
+
+
+        Point point_A = new Point(point_A_x, point_A_y);
+        Point point_B = new Point(point_B_x, point_B_y);
+
+        double distance = Point.distance(point_A, point_B);
+        System.out.println(String.format("%.2f", distance));
+
+        assert String.format("%.2f", distance).equals("12,73");
+        
+        Assert.assertEquals(String.format(
+                "%.2f", distance),
+                "12,73",
+                "Дистанция не верна фактическому результату"
+        );
+    }
+}
