8a8867fb86db3ac64d41f1c2cdd79860d8c908fd
yaroslavkorotyshov <ydkorotyshov@gmail.com>
2020-07-25 14:48:47 +0300

    lecture 2.4 - refactor

--- addressbook-web-tests/src/test/java/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/GroupCreationTests.java
@@ -12,40 +12,77 @@ public class GroupCreationTests {
   public void setUp() {
 
     // For Chrome
-    //System.setProperty("webdriver.chrome.wd","/Applications/Google Chrome.app/Contents/MacOS/chromedriver");
+    //System.setProperty("webdriver.chrome.driver","/Applications/Google Chrome.app/Contents/MacOS/chromedriver");
     //wd = new ChromeDriver();
 
-    System.setProperty("webdriver.gecko.wd","/Applications/Firefox.app/Contents/MacOS/geckodriver");
+    // For Firefox
+    System.setProperty("webdriver.gecko.driver","/Applications/Firefox.app/Contents/MacOS/geckodriver");
     wd = new FirefoxDriver();
 
     // Исполняемый драйвер расположил в пакете приложения по соответствующему пути
 
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    wd.get("http://localhost/addressbook/index.php");
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
   }
 
   @Test
   public void testGroupCreation() {
-    wd.get("http://localhost/addressbook/index.php");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("New_group_name_2");
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("Group_header_logo");
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("Group_footer_comment");
-    wd.findElement(By.name("submit")).click();
+
+    GroupData groupData = new GroupData(
+            "New_group_name_2",
+            "Group_header_logo",
+            "Group_footer_comment"
+    );
+
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(groupData);
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
     wd.findElement(By.linkText("group page")).click();
-    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() {
+    logout();
     wd.quit();
   }
 
